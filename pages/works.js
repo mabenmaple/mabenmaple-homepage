3ae2037693708fce5fe0204111b4b5fb6885d2cb
Nicolas Louw <nicolas.louw@student.unsw.edu.au>
2024-11-17 20:00:54 +1100

    fix

--- pages/works.js
+++ pages/works.js
@@ -1,16 +1,7 @@
 import {
     Container,
-    Heading,
-    SimpleGrid
+    Heading
 } from '@chakra-ui/react'
-import Section from '../components/section'
-import { WorkGridItem } from '../components/grid-item'
-import thumbOsuTrainer from '../public/images/works/osutrainer.png'
-import thumbMarkdownEditor from '../public/images/works/markdowneditor.png'
-import thumbTerminalMandelbrot from '../public/images/works/mandelbrot.png'
-import thumbDiscordBot from '../public/images/works/discordbot.png'
-import thumbPythonTyping from '../public/images/works/typing.png'
-import thumbHelloWorldHard from '../public/images/works/helloworld.png'
 
 const Works = () => {
     return (
